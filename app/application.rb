b21e4c3c7c2f117c4792727539b3d9ca97308911
Denise Pendleton <dpendl1@hotmail.com>
2018-04-07 21:22:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,18 @@ class Application
     req =  Rack::Request.new
 
     if req.path.match (/items/)
-
+      item_identifier = req.path.split("/items/").last
+      item = @@items.find{|item| item.name == item_identifier}
+      if item
+        resp.write "#{item.price}"
+      else
+        resp.status = 400
+        resp.write "Item not found"
 
     else
       resp.write "Route not found"
       resp.status = 404
     end
-    
+
   resp.finish
 end
