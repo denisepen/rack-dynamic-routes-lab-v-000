59bb2de00025df2350a74e27084aab6d7294a3a3
Denise Pendleton <dpendl1@hotmail.com>
2018-04-07 21:12:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,19 +11,15 @@ class Application
     else
       resp.write "Route not found"
       resp.status = 404
+    end
       # items = @@items.each do |key,value|
       #     key
       #   end
-      # 
+      #
       #   if items.include?(path.split.last)
       #     resp.write "#{items[:key]}"
       #   else
       #     resp.write "400"
-
-  end
-
-
-
 
   resp.finish
 end
