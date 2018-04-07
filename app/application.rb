74f6ee813ba67424049a4000b8624e7daa71378b
Denise Pendleton <dpendl1@hotmail.com>
2018-04-07 21:11:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,29 @@
 class Application
-  @@items = {apple: 1.00, orange: .50}
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
   def call(env)
     resp = Rack::Response.new
-    req =  Rack::Request.new 
+    req =  Rack::Request.new
 
     if req.path.match (/items/)
-      items = @@items.each do |key,value|
-          key
-        end
-        
-        if items.include?(path.split.last)
-          resp.write "#{items[:key]}"
+
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+      # items = @@items.each do |key,value|
+      #     key
+      #   end
+      # 
+      #   if items.include?(path.split.last)
+      #     resp.write "#{items[:key]}"
+      #   else
+      #     resp.write "400"
 
   end
 
 
 
 
-
+  resp.finish
 end
